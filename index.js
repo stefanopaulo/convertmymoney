889cd10a83dc1586d95f78eb01ae9db75a7fc277
stefanopaulo <stefanopaulo47@gmail.com>
2020-10-09 14:44:23 -0300

    adicionando funcionalidades e estilos

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
 
+const { convert, toDolar, toReal } = require('./lib/convert')
+
 const app = express()
 
 app.set('view engine', 'ejs')
@@ -10,6 +12,25 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/cotacao', (req, res) => {
+    const { cotacao, quantidade } = req.query
+    
+    if (Number(cotacao) && Number(quantidade)) {
+        const conversao = convert(cotacao, quantidade)
+    
+        return res.render('cotacao', {
+            error: false,
+            cotacao: toReal(cotacao),
+            quantidade: toDolar(quantidade),
+            conversao: toReal(conversao)
+        })
+    } else {
+        res.render('cotacao', {
+            error: 'Valores inválidos'
+        })
+    }
 })
 
 app.listen(3000, err => {
